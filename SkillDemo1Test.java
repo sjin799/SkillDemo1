fc91bdc724734509083b89b21a078522999dc4dc
sjin799 <goldiej1019@gmail.com>
2022-02-04 09:01:27 -0800

    Fixed test

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test 
     public void test1(){
-        assertEquals(3, SkillDemo1.minus(5, 1));
+        assertEquals(3, SkillDemo1.minus(5, 2));
     }
 }
